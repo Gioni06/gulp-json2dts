408e4372e95f71ea3fff8d537d683d5ddc1e6fb3
Jerry Zhang <jerryz@synnex.com>
2019-11-26 17:20:57 +0800

    fix: options passing issue

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp');
 var json2dts = require('../index');
 
-gulp.task('default', function () {
-	gulp.src('src/example.json')
-		.pipe(json2dts())
+gulp.task('default', function() {
+	gulp
+		.src('src/example.json')
+		.pipe(json2dts({ bannerComment: false }))
 		.pipe(gulp.dest('dist'));
 });
--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var through = require('through2');
 var generateSchema = require('generate-schema');
 var compiler = require('json-schema-to-typescript');
 
-module.exports = function (opts) {
+module.exports = function(opts) {
 	opts = opts || {};
 
-	return through.obj(function (file, enc, cb) {
+	return through.obj(function(file, enc, cb) {
 		if (file.isNull()) {
 			cb(null, file);
 			return;
@@ -20,15 +20,17 @@ module.exports = function (opts) {
 
 		try {
 			var fileName = file.relative.split('.')[0];
-			var jsonSchema = generateSchema.json(fileName, JSON.parse(file.contents.toString()));
-			var tsInterface = compiler.compile(jsonSchema);
-			file.path = gutil.replaceExtension(file.path, '.d.ts');
-			file.contents = new Buffer(tsInterface);
-			this.push(file);
+			var jsonSchema = generateSchema.json(
+				fileName,
+				JSON.parse(file.contents.toString())
+			);
+			compiler.compile(jsonSchema, '', opts).then(function(tsInterface) {
+				file.path = gutil.replaceExtension(file.path, '.d.ts');
+				file.contents = Buffer.from(tsInterface);
+				cb(null, file);
+			});
 		} catch (err) {
 			this.emit('error', new gutil.PluginError('gulp-json2dts', err));
 		}
-
-		cb();
 	});
 };
